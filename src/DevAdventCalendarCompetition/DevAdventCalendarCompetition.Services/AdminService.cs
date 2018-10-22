6ceb80bf9e20b13786ff51c8552df83534def178
Krzysztof Owsiany <gocom@o2.pl>
2018-10-22 23:56:43 +0200

    task_22 Add AutoMapper extession for integration with DI Change using Mapper. to _mapper from DI Move register Test and TestAnswert to separated Profile

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Services/AdminService.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Services/AdminService.cs
@@ -12,31 +12,36 @@ namespace DevAdventCalendarCompetition.Services
     {
         private readonly IAdminRepository _adminRepository;
         private readonly IBaseTestRepository _baseTestRepository;
+		private readonly IMapper _mapper;
 
-        public AdminService(IAdminRepository adminRepository, IBaseTestRepository baseTestRepository)
+        public AdminService(
+			IAdminRepository adminRepository, 
+			IBaseTestRepository baseTestRepository,
+			IMapper mapper)
         {
             _adminRepository = adminRepository;
             _baseTestRepository = baseTestRepository;
+			_mapper = mapper;
         }
 
         public List<TestDto> GetAllTests()
         {
             var testList = _adminRepository.GetAll();
-            var testDtoList = Mapper.Map<List<TestDto>>(testList);
+            var testDtoList = _mapper.Map<List<TestDto>>(testList);
             return testDtoList;
         }
 
         public TestDto GetTestById(int testId)
         {
             var test = _adminRepository.GetById(testId);
-            var testDto = Mapper.Map<TestDto>(test);
+            var testDto = _mapper.Map<TestDto>(test);
             return testDto;
         }
 
         public TestDto GetPreviousTest(int testNumber)
         {
             var test = _baseTestRepository.GetByNumber(testNumber);
-            var testDto = Mapper.Map<TestDto>(test);
+            var testDto = _mapper.Map<TestDto>(test);
             return testDto;
         }
 
@@ -44,14 +49,14 @@ namespace DevAdventCalendarCompetition.Services
         {
             testDto.StartDate = startTime;
             testDto.EndDate = endTime;
-            var test = Mapper.Map<Test>(testDto);
+            var test = _mapper.Map<Test>(testDto);
             _adminRepository.UpdateDates(test);
         }
 
         public void UpdateTestEndDate(TestDto testDto, DateTime endTime)
         {
             testDto.EndDate = endTime;
-            var test = Mapper.Map<Test>(testDto);
+            var test = _mapper.Map<Test>(testDto);
             _adminRepository.UpdateEndDate(test);
         }
 
