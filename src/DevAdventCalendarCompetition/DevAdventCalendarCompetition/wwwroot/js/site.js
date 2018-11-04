1735c0415418fb77ed12c797c2ce89139b1fafd6
Kamil Smieszek <kamilsmieszek@gmail.com>
2018-11-04 17:18:50 +0100

    Added background to current page navbar button.

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/wwwroot/js/site.js
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/wwwroot/js/site.js
@@ -1,1 +1,11 @@
 ﻿// Write your JavaScript code.
+
+$(function () {
+    var url = window.location.href;
+
+    $(".navbar-collapse.collapse a").each(function () {
+        if (url == (this.href)) {
+            $(this).closest("li").addClass("active");
+        }
+    });
+});    
