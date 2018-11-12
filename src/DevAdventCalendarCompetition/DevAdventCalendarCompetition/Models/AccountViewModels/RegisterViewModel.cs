23073b745a7aa33e1d6766539d5e15d446bd99b3
Krzysztof Owsiany <gocom@o2.pl>
2018-11-13 00:12:40 +0100

    Enable auth buttons in menu Translate register form Testing registration and login Centering registration forma and add santa animation

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/Models/AccountViewModels/RegisterViewModel.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/Models/AccountViewModels/RegisterViewModel.cs
@@ -10,14 +10,14 @@ namespace DevAdventCalendarCompetition.Models.AccountViewModels
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "Długość {0} powinna być większa niż {2} i mniejsza niż {1}.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "hasła")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Hasło potwierdzające")]
+        [Compare("Password", ErrorMessage = "Wprowadzone hasło i hasło potwierdzające nie są zgodne.")]
         public string ConfirmPassword { get; set; }
     }
 }
