11f3614a488d0a33da15c7c54f882a31027c5bc2
Kamil Śmieszek <kamilsmieszek@gmail.com>
2018-11-29 21:57:11 +0100

    Changed wrong answer of test saving code.

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/Controllers/TestController.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/Controllers/TestController.cs
@@ -19,19 +19,16 @@ namespace DevAdventCalendarCompetition.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(string answer = "")
+        public ActionResult Index(int testNumber, string answer = "")
         {
-            var testNumber = 1;
+            var test = _baseTestService.GetTestByNumber(testNumber);
+            var finalAnswer = answer.ToUpper().Replace(" ", "");
 
-            var fixedAnswer = answer.ToUpper().Replace(" ", "");
-
-            if (fixedAnswer != "0100111101000010010010100100010101000011010101000100100101010110010010010101010001011001")
+            if (test != null && finalAnswer != test.Answer)
             {
-                SaveWrongAnswer(fixedAnswer, testNumber);
+                SaveWrongAnswer(finalAnswer, testNumber);
 
-                ModelState.AddModelError("", "Odpowiedź jest nieprawidłowa. Spróbuj ponownie.");
-
-                var test = _baseTestService.GetTestByNumber(testNumber);
+                ModelState.AddModelError("", "Źle! Spróbuj ponownie :)");
 
                 return View("Index", test);
             }
