a17235ec8acc983df7c0b9cd41f5c9168eb3a685
Kamil Śmieszek <kamilsmieszek@gmail.com>
2019-01-10 14:47:09 +0100

    Removed async support from paginated list.

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Services/PaginatedList.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Services/PaginatedList.cs
@@ -24,13 +24,13 @@ namespace DevAdventCalendarCompetition.Services
 
         public bool HasNextPage => (PageIndex < TotalPages);
 
-        public static async Task<PaginatedList<T>> CreateAsync(
+        public static  PaginatedList<T> Create(
             IQueryable<T> source, int pageIndex, int pageSize)
         {
-            var count = await source.CountAsync();
-            var items = await source.Skip(
+            var count = source.Count();
+            var items = source.Skip(
                     (pageIndex - 1) * pageSize)
-                .Take(pageSize).ToListAsync();
+                .Take(pageSize).ToList();
             return new PaginatedList<T>(items, count, pageIndex, pageSize);
         }
     }
