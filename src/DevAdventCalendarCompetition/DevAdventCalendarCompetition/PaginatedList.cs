437643db5cd471c03c721ee7d2cbc9de78ea4141
Kamil Śmieszek <kamilsmieszek@gmail.com>
2019-01-12 12:46:44 +0100

    Added paginated list results view.

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/PaginatedList.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/PaginatedList.cs
@@ -11,11 +11,13 @@ namespace DevAdventCalendarCompetition
     {
         public int PageIndex { get; private set; }
         public int TotalPages { get; private set; }
+        public int PageSize { get; private set; }
 
         public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
         {
             PageIndex = pageIndex;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            PageSize = pageSize;
 
             this.AddRange(items);
         }
