0cc4b03f882752c9f89d53ef01776d663db23859
Kamil Śmieszek <kamilsmieszek@gmail.com>
2019-01-27 19:40:13 +0100

    Added puzzle test migrations after modifications.

---
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Repository/Migrations/20190127120458_PuzzleTestAnswerModification.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DevAdventCalendarCompetition.Repository.Migrations
+{
+    public partial class PuzzleTestAnswerModification : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlainAnswer",
+                table: "Test");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PlainAnswer",
+                table: "TestAnswer",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlainAnswer",
+                table: "TestAnswer");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PlainAnswer",
+                table: "Test",
+                nullable: true);
+        }
+    }
+}
