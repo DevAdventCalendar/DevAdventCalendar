4f63cd7581fe85d1ce51b5c8f5b04e26d8d940b1
Oliweirdo <olimatusik@gmail.com>
2019-08-14 20:48:11 +0200

    fixed issues according to code analysers

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/Vms/AccountViewModels/SendCodeViewModel.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/Vms/AccountViewModels/SendCodeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DevAdventCalendarCompetition.Vms
@@ -6,8 +7,11 @@ namespace DevAdventCalendarCompetition.Vms
     public class SendCodeViewModel
     {
         public string SelectedProvider { get; set; }
-        public ICollection<SelectListItem> Providers { get; set; }
-        public string ReturnUrl { get; set; }
+
+        public ICollection<SelectListItem> Providers { get; private set; }
+
+        public Uri ReturnUrl { get; set; }
+
         public bool RememberMe { get; set; }
     }
 }
