b4e2925d27ad7bda51cdab866c6e01ec2aff9383
Oliweirdo <olimatusik@gmail.com>
2019-08-14 20:51:18 +0200

    moved  class TotalTestResultEntryVmComparer to new file

---
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/Vms/TotalTestResultEntryVmComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DevAdventCalendarCompetition.Vms
+{
+
+    public class TotalTestResultEntryVmComparer : IEqualityComparer<TotalTestResultEntryVm>
+    {
+        public bool Equals(TotalTestResultEntryVm x, TotalTestResultEntryVm y)
+        {
+            return x.UserId == y.UserId;
+        }
+
+        public int GetHashCode(TotalTestResultEntryVm obj)
+        {
+            return obj.UserId.GetHashCode();
+        }
+    }
+}
