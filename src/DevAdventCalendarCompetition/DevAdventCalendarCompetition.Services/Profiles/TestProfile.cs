3c013ec030000c91d46157084fbdfc3b56806148
Maciej Skuratowski <maciejskuratowski@gmail.com>
2019-08-18 10:32:05 +0200

    Added and extended Automapper mappings. (#149)

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Services/Profiles/TestProfile.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Services/Profiles/TestProfile.cs
@@ -9,12 +9,27 @@ namespace DevAdventCalendarCompetition.Services.Profiles
         public TestProfile()
         {
             CreateMap<Test, TestDto>()
-                .ForMember(dest => dest.Answer, opt => opt.MapFrom(src => src.HashedAnswer));
+                .ForMember(dest => dest.Answer, opt => opt.MapFrom(src => src.HashedAnswer))
+                .ForMember(dest => dest.HasUserAnswered, opt => opt.MapFrom(src => src.HashedAnswer));
             CreateMap<TestDto, Test>()
                 .ForMember(dest => dest.Answers, opt => opt.Ignore())
-                .ForMember(dest => dest.HashedAnswer, opt => opt.Ignore());
+                .ForMember(dest => dest.HashedAnswer, opt => opt.Ignore())
+                .ForMember(dest => dest.WrongAnswers, opt => opt.Ignore());
+            CreateMap<TestAnswer, TestAnswerDto>()
+                .ForMember(dest => dest.AnsweringTime, opt => opt.MapFrom(src => src.AnsweringTime))
+                .ForMember(dest => dest.AnsweringTimeOffset, opt => opt.MapFrom(src => src.AnsweringTimeOffset))
+                .ForMember(dest => dest.TestId, opt => opt.MapFrom(src => src.TestId))
+                .ForMember(dest => dest.UserFullName, opt => opt.MapFrom(src => src.User.Email))
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId));
+            CreateMap<TestWrongAnswer, TestWrongAnswerDto>()
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
+                .ForMember(dest => dest.Answer, opt => opt.MapFrom(src => src.Answer))
+                .ForMember(dest => dest.TestId, opt => opt.MapFrom(src => src.TestId))
+                .ForMember(dest => dest.AnsweringTime, opt => opt.MapFrom(src => src.Time))
+                .ForMember(dest => dest.UserFullName, opt => opt.MapFrom(src => src.User.Email));
             CreateMap<Test, TestWithAnswerListDto>()
                 .ForMember(dest => dest.TestId, opt => opt.Ignore());
+
         }
     }
 }
