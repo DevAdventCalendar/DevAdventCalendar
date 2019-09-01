f5ed371f92f5b718e8a784aaa935600e32cfb384
OliWeirdo <oliweirdo@gmail.com>
2019-09-01 13:26:33 +0200

    fixed issues according to code analysers

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Services/Models/TestDto.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Services/Models/TestDto.cs
@@ -1,18 +1,8 @@
 ﻿using System;
+using DevAdventCalendarCompetition.Repository.Models;
 
 namespace DevAdventCalendarCompetition.Services.Models
 {
-    public enum TestStatus
-    {
-#pragma warning disable SA1602 // Enumeration items should be documented
-        NotStarted,
-
-        Started,
-
-        Ended
-#pragma warning restore SA1602 // Enumeration items should be documented
-    }
-
     public class TestDto
     {
         public int Id { get; set; }
@@ -46,13 +36,6 @@ namespace DevAdventCalendarCompetition.Services.Models
         public bool HasUserAnswered { get; set; }
 
         public bool IsAdvent => DateTime.Now.Month == 12 && DateTime.Now.Day < 25;
-
-        public static implicit operator TestDto(TestDto v)
-        {
-#pragma warning disable CA1065 // Do not raise exceptions in unexpected locations
-            throw new NotImplementedException();
-#pragma warning restore CA1065 // Do not raise exceptions in unexpected locations
-        }
 
         public TestDto ToTestDto()
         {
