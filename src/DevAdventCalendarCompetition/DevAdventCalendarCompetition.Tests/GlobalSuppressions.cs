90c7e6b1c1ad19e876cbd5bb6759d40e800fff2d
Patryk Lotzwi <plotzwi@gmail.com>
2019-09-24 19:42:16 +0200

    Fixed stylecop errors

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Tests/GlobalSuppressions.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Tests/GlobalSuppressions.cs
@@ -1,10 +1,7 @@
-﻿
-
-// This file is used by Code Analysis to maintain SuppressMessage 
+﻿// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
+// Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
-
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1413:Use trailing comma in multi-line initializers", Justification = "Reviewed.")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1200:Using directives should be placed correctly", Justification = "Reviewed.")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "Reviewed.")]
