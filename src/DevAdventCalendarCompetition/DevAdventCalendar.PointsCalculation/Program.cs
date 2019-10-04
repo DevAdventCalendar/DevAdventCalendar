810c7899cbc6b9fcc4f514f928ef99c65b7aefd4
OliWeirdo <oliweirdo@gmail.com>
2019-10-04 20:36:56 +0200

    Add new Project :DevAdventCalendarCompetition.PointsCalculation

---
+++ src/DevAdventCalendarCompetition/DevAdventCalendar.PointsCalculation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevAdventCalendarCompetition.PointsCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
