5bfd89aea94eb233e553aa72965a0a12092d8dce
Kamil Śmieszek <Kamil.Smieszek@me.gov.pl>
2019-11-06 13:19:54 +0100

    Fixed wrong interfaces implementation.

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.TestResultService/TestResultService.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.TestResultService/TestResultService.cs
@@ -6,65 +6,20 @@ using System.Threading.Tasks;
 namespace DevAdventCalendarCompetition.TestResultService
 {
     
-    public class TestResultService : ITestResultPlaceRule, ITestResultPointsRule, ITestResultRepository
+    public class TestResultService
     {
-        public void Calculate()
+        private ITestResultRepository _testResultRepository;
+
+        public TestResultService()
         {
-            throw new NotImplementedException();
+            this._testResultRepository = new TestResultRepository();
         }
 
-        public Task GetAnsweringTimeSum(DateTimeOffset dateFrom, DateTimeOffset dateTo)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task GetCorrectAnswersCount(DateTimeOffset dateFrom, DateTimeOffset dateTo)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void GetFinalResults()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task GetWeeklyResults(int WeekNumber)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task GetWrongAnswersCount(DateTimeOffset dateFrom, DateTimeOffset dateTo)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SaveUserFinalPlace(int userId, int Place)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SaveUserFinalScore(int userId, int Place)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SaveUserWeeklyPlace(int userId, int WeekNumber, int Place)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SaveUserWeeklyScore(int userId, int WeekNumber, int Score)
-        {
-            throw new NotImplementedException();
-        }
-
-        void  CalculateResults(DateTimeOffset dateFrom, DateTimeOffset dateTo) { }
+        void CalculateResults(DateTimeOffset dateFrom, DateTimeOffset dateTo) { }
 
         void CalculateWeeklyResults(int WeekNumber) { }
 
-        void SaveFinalResults() { }
-
-        void ITestResultRepository.SaveFinalResults()
+        void SaveFinalResults()
         {
             throw new NotImplementedException();
         }
