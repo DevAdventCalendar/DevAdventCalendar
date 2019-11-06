d5def3d3d4b2c3239c107979178872cc07c8197a
Kamil Śmieszek <Kamil.Smieszek@me.gov.pl>
2019-11-06 13:02:41 +0100

    Added test for result points calculation.

---
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Tests/ResultCalculationTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DevAdventCalendarCompetition.Repository.Models;
+using DevAdventCalendarCompetition.TestResultService;
+using Moq;
+using Xunit;
+
+namespace DevAdventCalendarCompetition.Tests
+{
+    public class ResultCalculationTest
+    {
+        private readonly List<TestAnswer> _answers = new List<TestAnswer>
+        {
+            new TestAnswer { Id = 1, UserId = "1", TestId = 1, AnsweringTime = DateTime.Now, AnsweringTimeOffset = default },
+            new TestAnswer { Id = 2, UserId = "1", TestId = 1, AnsweringTime = DateTime.Today.AddHours(-2), AnsweringTimeOffset = default },
+            new TestAnswer { Id = 3, UserId = "1", TestId = 2, AnsweringTime = DateTime.Today.AddHours(-1), AnsweringTimeOffset = default }
+        };
+
+        private Mock<CorrectAnswerPointsRule> _correctAnswersPointRuleMock;
+
+        public ResultCalculationTest()
+        {
+            this._correctAnswersPointRuleMock = new Mock<CorrectAnswerPointsRule>();
+        }
+
+        [Fact]
+        public void GetUserCorrectAnswers_ReturnPointsCount()
+        {
+            this._correctAnswersPointRuleMock.Setup(mock => mock.Calculate());
+        }
+    }
+}
