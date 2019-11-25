de18cc6d0f25ee8d3809ad373cdd82d15e4a8487
Patryk Lotzwi <plotzwi@gmail.com>
2019-11-25 22:40:27 +0100

    Extended ApplicationUser with notification preference fields

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Repository/Migrations/20191125210918_NotificationsPreferences.Designer.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Repository/Migrations/20191125210918_NotificationsPreferences.Designer.cs
@@ -4,14 +4,16 @@ using DevAdventCalendarCompetition.Repository.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DevAdventCalendarCompetition.Repository.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191125210918_NotificationsPreferences")]
+    partial class NotificationsPreferences
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
