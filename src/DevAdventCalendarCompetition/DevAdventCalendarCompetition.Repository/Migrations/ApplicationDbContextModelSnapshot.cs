22275a198ae6af6663cade96633ab283eea93e57
Kamil Śmieszek <kamilsmieszek@gmail.com>
2019-11-30 18:09:23 +0100

    Updated migration that creates new Results table.

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Repository/Migrations/ApplicationDbContextModelSnapshot.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Repository/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -80,25 +80,25 @@ namespace DevAdventCalendarCompetition.Repository.Migrations
                         .ValueGeneratedOnAdd()
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<int?>("FinalPlace");
+                    b.Property<int>("FinalPlace");
 
-                    b.Property<int?>("FinalPoints");
+                    b.Property<int>("FinalPoints");
 
                     b.Property<string>("UserId")
                         .IsRequired()
                         .HasMaxLength(450);
 
-                    b.Property<int?>("Week1Place");
+                    b.Property<int>("Week1Place");
 
-                    b.Property<int?>("Week1Points");
+                    b.Property<int>("Week1Points");
 
-                    b.Property<int?>("Week2Place");
+                    b.Property<int>("Week2Place");
 
-                    b.Property<int?>("Week2Points");
+                    b.Property<int>("Week2Points");
 
-                    b.Property<int?>("Week3Place");
+                    b.Property<int>("Week3Place");
 
-                    b.Property<int?>("Week3Points");
+                    b.Property<int>("Week3Points");
 
                     b.HasKey("Id");
 
