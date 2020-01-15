b8b6448dd0972dceb7e995ee02516670bc51d757
OliWeirdo <oliweirdo@gmail.com>
2020-01-15 22:19:49 +0100

    dispose of razor downgrade warning,  and authentication  sheme error

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/Vms/ManageViewsModels/ManageLoginsViewModel.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/Vms/ManageViewsModels/ManageLoginsViewModel.cs
@@ -10,8 +10,7 @@ namespace DevAdventCalendarCompetition.Vms
     {
         public IList<UserLoginInfo> CurrentLogins { get; private set; }
 
-        // TODO:  AuthenticationDescription applies only .Net Core 2.2 2.1 2.0 1.1 1.0
-        public IList<AuthenticationDescription> OtherLogins { get; private set; }
+        public IList<AuthenticationScheme> OtherLogins { get; private set; }
 
         public IList<AuthenticationScheme> ExternalProviders { get; private set; }
     }
