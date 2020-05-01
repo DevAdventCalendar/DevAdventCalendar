35a4145b358caf84bca1e94e0a3523704dd7eccd
Oliwia  Matusik <Oliweirdo@gmail.com>
2020-05-01 12:04:17 +0200

    Automatically unlocking the window without refreshing the page manually (#293)
    * add js logic to refresh element
    
    * removed unused changes from Index.cshtml
    
    Co-authored-by: WTobor <wero15@op.pl>

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/wwwroot/js/site.js
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Write your JavaScript code.
+// Write your JavaScript code.
 
 $(function () {
     var url = window.location.href;
@@ -34,3 +34,21 @@ function CheckTestStatus(testNumber) {
         $("#tile-open-alert").show();
     }
 }
+
+function refreshAt(hours, minutes, seconds) {
+    var now = new Date();
+    var then = new Date();
+
+    if (now.getHours() > hours ||
+        (now.getHours() == hours && now.getMinutes() > minutes) ||
+        now.getHours() == hours && now.getMinutes() == minutes && now.getSeconds() >= seconds) {
+        then.setDate(now.getDate() + 1);
+    }
+    then.setHours(hours);
+    then.setMinutes(minutes);
+    then.setSeconds(seconds);
+
+    var timeout = (then.getTime() - now.getTime());
+    setTimeout(function () { window.location.reload(true); }, timeout);
+}
+refreshAt(20, 00, 0); //Will refresh the page at 20:00pm
