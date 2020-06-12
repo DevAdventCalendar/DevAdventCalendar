f269106436a1434264d666766f78bc15322a3896
Grzegorz Mutkowski <grzegorz.mutkowski@gmail.com>
2020-06-12 13:29:31 +0200

    created DelayedAnswer message

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/Resources/ViewsTestMessages.Designer.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/Resources/ViewsTestMessages.Designer.cs
@@ -61,6 +61,15 @@ namespace DevAdventCalendarCompetition.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Niestety, spóźniłeś się z odpowiedzią na to pytanie :(.
+        /// </summary>
+        public static string DelayedAnswer {
+            get {
+                return ResourceManager.GetString("DelayedAnswer", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Ho ho! Już odpowiedziałeś na to pytanie, brawo :).
         /// </summary>
         public static string HasUserAnsweredQuestion {
