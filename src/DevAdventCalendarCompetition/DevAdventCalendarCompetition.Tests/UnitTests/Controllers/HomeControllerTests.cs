9d80018d96ff7c37437d230595e6fd956db5a75e
Grzegorz Mutkowski <grzegorz.mutkowski@gmail.com>
2020-08-25 14:39:13 +0200

    Add tests for index method in home controller

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Tests/UnitTests/Controllers/HomeControllerTests.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Tests/UnitTests/Controllers/HomeControllerTests.cs
@@ -1,10 +1,87 @@
 using System;
 using System.Collections.Generic;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Text;
+using DevAdventCalendarCompetition.Controllers;
+using DevAdventCalendarCompetition.Models.Test;
+using DevAdventCalendarCompetition.Repository.Models;
+using DevAdventCalendarCompetition.Services.Interfaces;
+using DevAdventCalendarCompetition.Services.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+using static DevAdventCalendarCompetition.Tests.TestHelper;
 
 namespace DevAdventCalendarCompetition.Tests.UnitTests.Controllers
 {
     public class HomeControllerTests
     {
+        private readonly Mock<IHomeService> _homeServiceMock;
+
+        public HomeControllerTests()
+        {
+            this._homeServiceMock = new Mock<IHomeService>();
+        }
+
+        [Fact]
+        public void Index_CurrentTestDtoIsNull_ReturnsEmptyViewResult()
+        {
+            // Arrange
+            this._homeServiceMock.Setup(x => x.GetCurrentTests()).Returns((List<TestDto>)null);
+            using var controller = new HomeController(this._homeServiceMock.Object);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.Model);
+        }
+
+        [Fact]
+        public void Index_UserIsNull_ReturnsAViewResutWithListOfTestDto()
+        {
+            // Arrange
+            var currentTestList = new List<TestDto>()
+            {
+                GetTestDto()
+            };
+            this._homeServiceMock.Setup(x => x.GetCurrentTests()).Returns(currentTestList);
+            using var controller = new HomeController(this._homeServiceMock.Object);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.IsType<List<TestDto>>(viewResult.ViewData.Model);
+        }
+
+        private static TestViewModel GetTestViewModel() => new TestViewModel
+        {
+            Number = 0,
+            Description = "Description",
+            StartDate = DateTime.Now,
+            EndDate = DateTime.Now.AddDays(1),
+            Answers = new List<string>()
+            {
+                "Answer"
+            },
+            SponsorName = null,
+            SponsorLogoUrl = null,
+            Discount = null,
+            DiscountUrl = null,
+            DiscountLogoUrl = null,
+            DiscountLogoPath = null
+        };
+
+        private static TestDto GetTest(TestStatus status)
+        {
+            var test = GetTestDto();
+            test.Status = status;
+            return test;
+        }
     }
 }
