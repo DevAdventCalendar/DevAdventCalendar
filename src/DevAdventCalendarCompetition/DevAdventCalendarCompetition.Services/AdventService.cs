ab2cbd6349569ac5c4dd150b73ffbb4b1e983119
Mateusz Golombniak <Mejtika@hotmail.com>
2020-09-02 12:11:16 +0200

    Add new AdventService

---
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Services/AdventService.cs
@@ -0,0 +1,23 @@
+using DevAdventCalendarCompetition.Services.Interfaces;
+using DevAdventCalendarCompetition.Services.Options;
+
+namespace DevAdventCalendarCompetition.Services
+{
+    public class AdventService : IAdventService
+    {
+        private readonly AdventSettings _adventSettings;
+        private readonly IDateTimeService _dateTimeService;
+
+        public AdventService(AdventSettings adventSettings, IDateTimeService dateTimeService)
+        {
+            this._adventSettings = adventSettings;
+            this._dateTimeService = dateTimeService;
+        }
+
+        public bool IsAdvent()
+        {
+            var now = this._dateTimeService.Now;
+            return this._adventSettings.StartDate <= now && this._adventSettings.EndDate >= now;
+        }
+    }
+}
