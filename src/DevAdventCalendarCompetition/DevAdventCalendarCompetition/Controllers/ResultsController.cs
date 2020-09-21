94d54ffeba9ecc93ae980966035f4ce29f121410
programmer-girl <wero15@op.pl>
2020-09-21 11:11:28 +0200

    Bugfix/swagger fix (#382)
    * add default route in Startup
    
    * adding route attributes
    
    Co-authored-by: Grzegorz Mutkowski <grzegorz.mutkowski@gmail.com>

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/Controllers/ResultsController.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition/Controllers/ResultsController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DevAdventCalendarCompetition.Controllers
 {
+    [Route("[controller]/[action]")]
     public class ResultsController : Controller
     {
         private readonly IResultsService _resultsService;
@@ -19,6 +20,7 @@ namespace DevAdventCalendarCompetition.Controllers
             this._resultsService = resultsService ?? throw new ArgumentNullException(nameof(resultsService));
         }
 
+        [HttpGet]
         public ActionResult Index(int? pageIndex)
         {
             var userId = this.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
