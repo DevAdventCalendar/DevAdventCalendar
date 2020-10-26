9d6e922790702b1bbedfb9657288607dcaf3ea24
Kamil Śmieszek <kamilsmieszek@gmail.com>
2020-10-26 10:39:11 +0100

    Added new cases to answer parsing tests. Renamed the parse test method.

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Tests/UnitTests/AnswerServiceTest.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Tests/UnitTests/AnswerServiceTest.cs
@@ -13,13 +13,16 @@ namespace DevAdventCalendarCompetition.Tests.UnitTests
             {
                 new object[] { "Test answer", "TEST ANSWER" },
                 new object[] { "  Test answer  ", "TEST ANSWER" },
-                new object[] { "    Test     answer    ", "TEST ANSWER" }
+                new object[] { "    Test     answer    ", "TEST ANSWER" },
+                new object[] { " test answer test answer ", "TEST ANSWER TEST ANSWER" },
+                new object[] { "TEST    answer     ", "TEST ANSWER" },
+                new object[] { "        Test    answer   Test answer TEST", "TEST ANSWER TEST ANSWER TEST" },
             };
         }
 
         [Theory]
         [MemberData(nameof(SpacesReplaceData))]
-        public void InUserAnswer_ShouldReplaceWhitespacesToSingleSpace(string userAnswer, string resultAnswer)
+        public void ParseTestAnswer_ShouldReplaceWhitespacesToSingleSpace(string userAnswer, string resultAnswer)
         {
             var sut = new AnswerService();
 
