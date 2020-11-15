c14e29b94a303aabf3c44aed7a24f818c10a400e
smiechuskce <kamilsmieszek@gmail.com>
2020-11-15 11:48:54 +0100

    Changed existing users UserNames to random strings (#428)
    * Added test for AccountController.
    
    * Added notification for user about possibility to change his user name.
    
    * Added migration with script updating user names to the random strings.
    
    * Changed user name change notification.
    
    * Added user name change notification to resources.

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Tests/UnitTests/Controllers/AccountControllerTests.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Tests/UnitTests/Controllers/AccountControllerTests.cs
@@ -9,7 +9,10 @@ using Castle.Core.Logging;
 using DevAdventCalendarCompetition.Controllers;
 using DevAdventCalendarCompetition.Models.Account;
 using DevAdventCalendarCompetition.Repository.Models;
+using DevAdventCalendarCompetition.Resources;
+using DevAdventCalendarCompetition.Services;
 using DevAdventCalendarCompetition.Services.Interfaces;
+using DevAdventCalendarCompetition.Tests.UnitTests.Controllers.IdentityHelpers;
 using FluentAssertions;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
@@ -30,11 +33,42 @@ namespace DevAdventCalendarCompetition.Tests.UnitTests.Controllers
     {
         private readonly Mock<IAccountService> _accountServiceMock;
         private readonly Mock<ILogger<AccountController>> _loggerMock;
+        private IUserValidator<ApplicationUser> _userValidator;
+        private Mock<IUserPasswordStore<ApplicationUser>> _userStore;
+        private IAccountService _accountService;
+        private ILogger<AccountController> _logger;
 
         public AccountControllerTests()
         {
             this._accountServiceMock = new Mock<IAccountService>();
             this._loggerMock = new Mock<ILogger<AccountController>>();
+            this._userValidator = new UserValidator<ApplicationUser>();
+            this._logger = new Mock<ILogger<AccountController>>().Object;
+        }
+        
+        [Fact]
+        public void Register_CannotAddUserWithExistingUsername_ThrowsException()
+        {
+            this._userStore = new Mock<IUserPasswordStore<ApplicationUser>>();
+            this._userStore.Setup(x => x.GetUserNameAsync(It.IsAny<ApplicationUser>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult("user1"));
+
+            using var userManager = new FakeUserManager(this._userValidator, this._userStore.Object);
+            this._accountService = new AccountService(null, userManager, null);
+
+            var registerViewModel = new RegisterViewModel
+            {
+                UserName = "user1",
+                Email = "user1@gmail.com",
+                Password = "userP@ssword1",
+                ConfirmPassword = "userP@ssword1",
+            };
+            using var controller = this.CreateAccountController();
+
+            Func<IActionResult> result = () => controller.Register(registerViewModel).Result;
+
+            var exception = Assert.Throws<InvalidOperationException>(result);
+            Assert.Equal(exception.Message, ExceptionsMessages.UserWithNameExists);
         }
 
         [Fact]
@@ -720,6 +754,11 @@ namespace DevAdventCalendarCompetition.Tests.UnitTests.Controllers
                 .Returns(authServiceMock.Object);
 
             return serviceProviderMock.Object;
+        }
+        
+        private AccountController CreateAccountController()
+        {
+            return new AccountController(this._accountService, this._logger);
         }
     }
 }
