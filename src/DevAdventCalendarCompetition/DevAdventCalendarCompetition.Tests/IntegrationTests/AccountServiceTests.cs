b68e533e30be0c179a4b9523b8c2c5df6d87d706
Mateusz Golombniak <Mejtika@hotmail.com>
2020-11-18 19:34:52 +0100

    Add AccountService tests

---
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Tests/IntegrationTests/AccountServiceTests.cs
@@ -0,0 +1,91 @@
+using System.Threading.Tasks;
+using DevAdventCalendarCompetition.Repository.Models;
+using DevAdventCalendarCompetition.Services.Interfaces;
+using DevAdventCalendarCompetition.Tests.IntegrationTests.TestStartup;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Xunit;
+using static DevAdventCalendarCompetition.Tests.IntegrationTests.TestStartup.StartupTestBase;
+
+namespace DevAdventCalendarCompetition.Tests.IntegrationTests
+{
+    public class AccountServiceTests : IntegrationStartupTestBase
+    {
+        [Fact]
+        public async Task CreateAsync_CannotAddUserWithExistingUsernameAndEmail()
+        {
+            // Arrange
+            var email = "test@mail.com";
+            var userName = "testUser";
+
+            await AddApplicationUserAsync(new ApplicationUser
+            {
+                Email = email,
+                UserName = userName
+            }).ConfigureAwait(false);
+
+            // Act
+            var result = await ExecuteAsync<IAccountService, IdentityResult>(accountService
+                => accountService.CreateAsync(
+                    new ApplicationUser
+                    {
+                        Email = email,
+                        UserName = userName,
+                    }, "P@ssw0rd")).ConfigureAwait(false);
+
+            // Assert
+            result.Should().NotBe(IdentityResult.Success);
+            result.Errors.Should().HaveCount(2);
+            result.Errors.Should().Contain(x => x.Code == "DuplicateEmail");
+            result.Errors.Should().Contain(x => x.Code == "DuplicateUserName");
+        }
+
+        [Fact]
+        public async Task CreateAsync_CannotAddUserWithExistingUsername()
+        {
+            var userName = "testUser";
+
+            await AddApplicationUserAsync(new ApplicationUser
+            {
+                Email = "test@mail.com",
+                UserName = userName,
+            }).ConfigureAwait(false);
+
+            var result = await ExecuteAsync<IAccountService, IdentityResult>(accountService
+                => accountService.CreateAsync(
+                    new ApplicationUser
+                    {
+                        Email = "test2@mail.com",
+                        UserName = userName,
+                    }, "P@ssw0rd")).ConfigureAwait(false);
+
+            result.Should().NotBe(IdentityResult.Success);
+            result.Errors.Should().HaveCount(1);
+            result.Errors.Should().Contain(x => x.Code == "DuplicateUserName");
+        }
+
+        [Fact]
+        public async Task CreateAsync_CannotAddUserWithExistingEmail()
+        {
+            var email = "test@mail.com";
+
+            await AddApplicationUserAsync(new ApplicationUser
+            {
+                Email = email,
+                UserName = "testUser",
+            }).ConfigureAwait(false);
+
+            var result = await ExecuteAsync<IAccountService, IdentityResult>(accountService
+                => accountService.CreateAsync(
+                    new ApplicationUser
+                    {
+                        Email = email,
+                        UserName = "testUser2",
+                    }, "P@ssw0rd")).ConfigureAwait(false);
+
+            result.Should().NotBe(IdentityResult.Success);
+            result.Errors.Should().HaveCount(1);
+            result.Errors.Should().Contain(x => x.Code == "DuplicateEmail");
+        }
+    }
+}
