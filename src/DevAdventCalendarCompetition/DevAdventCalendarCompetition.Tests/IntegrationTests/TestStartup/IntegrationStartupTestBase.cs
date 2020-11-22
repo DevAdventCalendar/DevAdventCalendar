05b8b4cda767b2607f4f4f7fa9309a28afa90418
Kamil Śmieszek <kamilsmieszek@gmail.com>
2020-11-22 13:36:13 +0100

    Added mutex to integration test base.

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Tests/IntegrationTests/TestStartup/IntegrationStartupTestBase.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Tests/IntegrationTests/TestStartup/IntegrationStartupTestBase.cs
@@ -1,13 +1,33 @@
 using System.Threading.Tasks;
+using Nito.AsyncEx;
 using Xunit;
 
 namespace DevAdventCalendarCompetition.Tests.IntegrationTests.TestStartup
 {
     public abstract class IntegrationStartupTestBase : IAsyncLifetime
     {
+        private static readonly AsyncLock Mutex = new AsyncLock();
+
+        private static bool _initialized;
+
         public virtual async Task InitializeAsync()
         {
-            await StartupTestBase.ResetCheckpoint().ConfigureAwait(false);
+            if (_initialized)
+            {
+                return;
+            }
+
+            using (await Mutex.LockAsync())
+            {
+                if (_initialized)
+                {
+                    return;
+                }
+
+                await StartupTestBase.ResetCheckpoint().ConfigureAwait(false);
+
+                _initialized = true;
+            }
         }
 
         public virtual Task DisposeAsync() => Task.CompletedTask;
