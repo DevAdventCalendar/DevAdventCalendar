5ce0b0c4328782f889badb21a0bba104e5a33c1f
Kamil Śmieszek <kamilsmieszek@gmail.com>
2020-12-22 18:55:03 +0100

    Counting only the answers added only in the same week as week when the test started.

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Repository/UserTestAnswersRepository.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Repository/UserTestAnswersRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using DevAdventCalendarCompetition.Repository.Context;
 using DevAdventCalendarCompetition.Repository.Interfaces;
 using DevAdventCalendarCompetition.Repository.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevAdventCalendarCompetition.Repository
 {
@@ -62,34 +63,45 @@ namespace DevAdventCalendarCompetition.Repository
         {
             return this._dbContext
                 .UserTestCorrectAnswers
+                .Include(a => a.Test)
                 .Where(a => a.AnsweringTime.CompareTo(dateFrom.DateTime) >= 0 &&
                             a.AnsweringTime.CompareTo(dateTo.DateTime) < 0)
                 .AsEnumerable()
                 .GroupBy(a => a.UserId)
-                .ToDictionary(ug => ug.Key, ug => ug.Select(t => t.TestId).Distinct().Count());
+                .ToDictionary(ug => ug.Key, ug =>
+                    ug.Count(t => t.Test.StartDate.Value >= dateFrom && t.Test.StartDate.Value < dateTo));
         }
 
         public IDictionary<string, double> GetAnsweringTimeSumPerUserForDateRange(DateTimeOffset dateFrom, DateTimeOffset dateTo)
         {
             return this._dbContext
                 .UserTestCorrectAnswers
+                .Include(a => a.Test)
                 .Where(a => a.AnsweringTime.CompareTo(dateFrom.DateTime) >= 0 &&
                             a.AnsweringTime.CompareTo(dateTo.DateTime) < 0)
                 .AsEnumerable()
                 .GroupBy(a => a.UserId)
-                .Select(a => new { UserId = a.Key, Tests = a.Select(b => new { b.TestId, b.AnsweringTimeOffset }).GroupBy(b => b.TestId) })
-                .ToDictionary(ug => ug.UserId, ug => Math.Round(ug.Tests.Sum(x => x.OrderBy(a => a.AnsweringTimeOffset).First().AnsweringTimeOffset.TotalSeconds), 2));
+                .Select(a => new
+                {
+                    UserId = a.Key, Tests = a
+                    .Where(t => t.Test.StartDate.Value >= dateFrom && t.Test.StartDate.Value < dateTo)
+                    .Select(b => new { b.TestId, b.AnsweringTimeOffset }).GroupBy(b => b.TestId)
+                })
+                .ToDictionary(ug => ug.UserId, ug =>
+                    Math.Round(ug.Tests.Sum(x => x.OrderBy(a => a.AnsweringTimeOffset).First().AnsweringTimeOffset.TotalSeconds), 2));
         }
 
         public IDictionary<string, int> GetWrongAnswersPerUserForDateRange(DateTimeOffset dateFrom, DateTimeOffset dateTo)
         {
             return this._dbContext
                 .UserTestWrongAnswers
+                .Include(a => a.Test)
                 .Where(a => a.Time.CompareTo(dateFrom.DateTime) >= 0 &&
                             a.Time.CompareTo(dateTo.DateTime) < 0)
                 .AsEnumerable()
                 .GroupBy(a => a.UserId)
-                .ToDictionary(ug => ug.Key, ug => ug.Count());
+                .ToDictionary(ug => ug.Key, ug =>
+                    ug.Count(t => t.Test.StartDate.Value >= dateFrom && t.Test.StartDate.Value < dateTo));
         }
     }
 }
