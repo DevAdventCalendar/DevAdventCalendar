0570b6b2895f9e0aba7dc584bbea2be279583e5b
Krzysztof <morzansiera@gmail.com>
2021-01-31 19:48:00 +0100

    Displaying wrong answers instead of number of wrong answers

--- src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Services/Models/StatisticsDto.cs
+++ src/DevAdventCalendarCompetition/DevAdventCalendarCompetition.Services/Models/StatisticsDto.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace DevAdventCalendarCompetition.Services.Models
 {
@@ -7,6 +8,10 @@ namespace DevAdventCalendarCompetition.Services.Models
         public int WrongAnswerCount { get; set; }
 
         public DateTime? CorrectAnswerDateTime { get; set; }
+
+#pragma warning disable CA2227 // Collection properties should be read only
+        public List<string> WrongAnswers { get; set; }
+#pragma warning restore CA2227 // Collection properties should be read only
 
         public int TestNumber { get; set; }
     }
